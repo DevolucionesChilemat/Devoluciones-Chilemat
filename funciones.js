dc37a57709c39aab8e2586b35368ec878e54b6f7
Manuel Diaz <Mana.diaz@duocuc.cl>
2022-01-25 00:22:04 -0300

    main

---
+++ funciones.js
@@ -0,0 +1,54 @@
+
+/*5 FUNCIONES*/
+
+
+var validaNombre = function(){
+    var nom = document.getElementById("formulario_nombre").value;
+    if(nom.length>0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+var validarTelefono = function(){
+    var tel = document.getElementById("formulario_despacho").value;
+    if(tel.length==5){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+var validarMensaje = function(){
+    var men = document.getElementById("formulario_detalle").value;
+    if(men.length>3){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+// validacion de funciones 
+
+var validarFunciones =function(){
+    if(validaNombre()==true){
+    }else{
+        envio="El campo Nombre Cliente debe ser ingresado";
+        return envio;
+
+    }if(validarTelefono()==true){
+    }else{
+        envio="El campo Guía de despacho debe contener al menos 5 dígitos";
+        return envio;
+    }
+    if(validarMensaje()==true){
+    }else{
+        envio="El campo Detalle debe contener como mínimo de información";
+        return envio;
+    }
+    if(validaNombre()==true&&validarTelefono()==true&&validarCorreo()==true&&validarMensaje()==true){
+        envio="FORMULARIO DE CONSULTA SE HA ENVIADO CON ÉXITO";
+        return envio;
+    }
+}
